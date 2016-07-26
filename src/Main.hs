5e3c4d6765718fa343c54aa69d3c3edd70ef9b66
Jan-Philip Loos <maxdaten@gmail.com>
2016-07-26 11:12:10 +0200

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Reflex.Dom
+
+main :: IO ()
+main = mainWidget $ el "div" $ do
+  el "h1" $ text "reflex-starter"
+  el "a" $ text "Now you can start playing with Reflex!"
+  el "div" $ do
+      t <- textInput def
+      text "Last key pressed: "
+      let keypressEvent = fmap show $ _textInput_keypress t
+      keypressDyn <- holdDyn "None" keypressEvent
+      dynText keypressDyn
